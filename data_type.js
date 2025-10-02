1498c3c18da095dba83e75053176087c26ee8de4
Adeteju001 <adegokeadeteju001@gmail.com>
2025-10-02 22:07:28 +0100

    learning

---
+++ data_type.js
@@ -0,0 +1,9 @@
+let radius = 5
+const pi = 3.14
+let area
+
+radius = 7
+
+area = pi * radius * radius
+
+console.log(area)
---
+++ variable.js
@@ -0,0 +1,11 @@
+let num = 4
+console.log(num)
+
+let username = 'teju'
+console.log(username + num)
+
+num = 5
+console.log(3 + num)
+
+let username1 = 'teju'
+console.log(username1 + num)
