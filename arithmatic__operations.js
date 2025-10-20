997d14daadd099b9a687959e2fe3ac9ee4ab8887
Adeteju001 <adegokeadeteju001@gmail.com>
2025-10-20 18:13:33 +0100

    learning

---
+++ arithmatic__operations.js
@@ -0,0 +1,29 @@
+let  num1 = 5
+
+let num2 = 8
+
+let result = num1 + num2
+console.log(result);
+
+let  num1 = true
+
+let num2 = true
+
+let result1 = num1 + num2
+console.log(result1);
+
+let num = 5
+
+let x = num++  // post increment
+let y = ++num  // pre increment
+
+console.log(x, y, num);
+
+let result3 = Math.pow(4,3)
+console.log(result3);
+
+let result4 = num ** num
+console.log(result4);
+
+let result5 = 5 ** 5
+console.log(result5);
---
+++ relational_operators.js
@@ -0,0 +1,10 @@
+let x = 5
+let y = 8
+
+let data = x >= y
+let data1 = x == y // check for value only
+
+let z = '5'
+let data2 = x === z // check both value and type
+
+console.log(data, data1, data2);
