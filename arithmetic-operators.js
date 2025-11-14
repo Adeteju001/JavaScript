c0800393ef998e284f89cd486cd56e0b26af6c23
Adeteju001 <adegokeadeteju001@gmail.com>
2025-11-14 13:19:57 +0100

    learning

---
+++ arithmetic-operators.js
@@ -0,0 +1,24 @@
+let a = 5;
+let b = 3;
+
+console.log(" Addittion:" , a + b);
+console.log(" Subtraction:" , a - b);
+console.log(" Multiplication:" , a * b);
+console.log(" Division:" , a / b);
+console.log(" Modulus:" , a % b);
+console.log(" Exponential:" , a ** b);
+
+console.log("50" + 5);
+console.log(50 +  5);
+console.log("50" - 5);
+console.log("50" / 5);
+console.log("50" * 5);
+console.log("50" % 5);
+console.log("50" ** 5);
+
+console.log(a++);
+console.log(++a);
+console.log(a--);
+console.log(--a);
+
+console.log( "A" + "B")
